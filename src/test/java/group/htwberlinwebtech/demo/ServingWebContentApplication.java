f9e8c2f7f42acb7ba69f97f2e269e3097ddd3ae9
Vidhya Sew <v.sew1999@gmail.com>
2022-10-17 18:22:31 +0200

    run application class

---
+++ src/test/java/group/htwberlinwebtech/demo/ServingWebContentApplication.java
@@ -0,0 +1,12 @@
+package group.htwberlinwebtech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+    
+}
